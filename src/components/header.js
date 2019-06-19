80154b43a0be0ebbe874407f418552fb4b96ac1d
marcuspereiradev <vinnycoast@gmail.com>
2019-06-19 15:53:18 -0300

    Header component

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/header.scss';
+
+const Header = () => (
+  <header className='header'>
+    <h1>Quanto vale meu carro?</h1>
+    <p>Um jeito fácil e tranquilo de saber</p>
+  </header>
+)
+
+export default Header;
