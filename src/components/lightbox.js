b7b74ace58465eb11ddc7838ff9eca77ffebd7eb
marcuspereiradev <vinnycoast@gmail.com>
2019-06-19 20:09:07 -0300

    Add lightbox component

---
+++ src/components/lightbox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/lightbox.scss';
+
+const Lightbox = ({ car_information }) => (
+  <div className='lightbox'>
+    <div className='lightbox-content'>
+      <div className='lightbox-items'>
+        <h1>{car_information.marca}</h1>
+        <p>{car_information.name} {car_information.combustivel} {car_information.ano_modelo}</p>
+        <p className='price'>{car_information.preco}</p>
+      </div>
+    </div>
+  </div>
+)
+
+export default Lightbox;
