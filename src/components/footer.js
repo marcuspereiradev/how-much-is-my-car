e5594f8a6a7a543d3228cdbc320b396cc93eb7e9
marcuspereiradev <vinnycoast@gmail.com>
2019-06-19 15:55:32 -0300

    Footer component

---
+++ src/components/footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/footer.scss';
+
+const Footer = () => (
+  <footer className='footer'>created by <a href='https://github.com/marcuspereiradev/how-much-is-my-car' >Marcus Pereira</a></footer>
+)
+
+export default Footer;
