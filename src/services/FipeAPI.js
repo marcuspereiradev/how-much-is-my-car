ac19753ff14d1cb215885e9b918220c61e835569
marcuspereiradev <vinnycoast@gmail.com>
2019-06-28 13:51:43 -0300

    Improving methods adding error verification

--- src/services/FipeAPI.js
+++ src/services/FipeAPI.js
@@ -1,27 +1,52 @@
 class FipeAPI {
 
   static async fetchCarBrands() {
-    const response = await fetch('http://fipeapi.appspot.com/api/1/carros/marcas.json');
-    const data = await response.json();
-    return data;
+    try {
+      const response = await fetch('http://fipeapi.appspot.com/api/1/carros/marcas.json');
+      const data = await response.json();
+      return data;
+    }
+    catch(err) {
+      console.error(`Something went wrong in fetchCarBrands ${err}`);
+      return [];
+    }
   }
 
   static async fetchCarModels(brandId) {
-    const response = await fetch(`http://fipeapi.appspot.com/api/1/carros/veiculos/${brandId}.json`);
-    const data = await response.json();
-    return data;
+    try {
+      const response = await fetch(`http://fipeapi.appspot.com/api/1/carros/veiculos/${brandId}.json`);
+      const data = await response.json();
+      return data;
+    }
+    catch(err) {
+      console.error(`Something went wrong in fetchCarModels ${err}`);
+      return [];
+    }
   }
 
   static async fetchCarYears(brandId, modelId) {
-    const response = await fetch(`http://fipeapi.appspot.com/api/1/carros/veiculo/${brandId}/${modelId}.json`);
-    const data = await response.json();
-    return data;
+    try {
+      const response = await fetch(`http://fipeapi.appspot.com/api/1/carros/veiculo/${brandId}/${modelId}.json`);
+      const data = await response.json();
+      return data;
+    }
+    catch(err) {
+      console.error(`Something went wrong in fetchCarYears ${err}`);
+      return [];
+    }
   }
 
   static async fetchCarInformation(brandId, modelId, yearId) {
-    const response = await fetch(`http://fipeapi.appspot.com/api/1/carros/veiculo/${brandId}/${modelId}/${yearId}.json`);
-    const data = await response.json();
-    return data;
+    try {
+      const response = await fetch(`http://fipeapi.appspsot.com/api/1/carros/veiculo/${brandId}/${modelId}/${yearId}.json`);
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }
+    catch(err) {
+      console.error("Something went wrong in fetchCarInformation", err);
+      return {};
+    }
   }
 }
 
