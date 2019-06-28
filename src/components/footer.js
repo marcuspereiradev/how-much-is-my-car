b36f8a6e2e2be35aa11665975e6c60f7a5a9e684
marcuspereiradev <vinnycoast@gmail.com>
2019-06-28 13:47:06 -0300

    Getting current year

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import '../styles/footer.scss';
 
 const Footer = () => (
   <footer className='footer'>
-    &copy; 2019 created by <a href='https://github.com/marcuspereiradev/how-much-does-cost-my-car' >Marcus Pereira</a>
+    &copy; {new Date().getFullYear()} created by <a href='https://github.com/marcuspereiradev/how-much-does-cost-my-car' >Marcus Pereira</a>
   </footer>
 )
 
